e9d9b7d7dec1f8c9e52184b385768cf46dae6357
sahilBastola <teknathbastola@gmail.com>
2021-02-23 11:22:25 +0545

    Initial commit

---
+++ paythonlab11.py
@@ -0,0 +1,6 @@
+#1.write a program that takes thre numbers and print their sum . every number is given on a seperete line.
+a=int(input("inter the value of first number"))
+b=int(input("inter the value of second number"))
+c=int(input("inter the value of third number"))
+sum=a+b
+print('the sum of all given number',sum)
