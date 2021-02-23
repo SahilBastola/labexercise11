9e03e786f10336e00da61042bdb6d7fafa0c04cc
sahilBastola <teknathbastola@gmail.com>
2021-02-23 11:42:56 +0545

    first question

---
+++ paython lab question 2.py
@@ -0,0 +1,7 @@
+#2.write a program reads the length of the base and the hight of a right angle triangle and print the area every number is given on a seperate line.
+hight=float(input("enter the hight of the triangle"))
+length=float(input("enter the length of the triangle"))
+area=(hight*length)//2
+#the float division // round the result down to the nearest whole nimber
+
+print(f'the area of right angle triangle is{area}')
